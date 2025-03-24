ad4a780c50791a6344068074913027b097d4995f
Mohamed-AIT-LAHCEN <mohamedaitlahcen004@gmail.com>
2025-03-24 14:25:05 +0000

    Update movie.h

--- TP8/movie.h
+++ TP8/movie.h
@@ -1,1 +1,33 @@
+#ifndef MOVIE_H
+#define MOVIE_H
 
+#include <string>
+
+class Movie {
+private:
+    std::string name;
+    std::string rating;
+    int watched;
+    
+public:
+    // Constructeur
+    Movie(std::string name, std::string rating, int watched);
+    
+    // Getters
+    std::string get_name() const;
+    std::string get_rating() const;
+    int get_watched() const;
+    
+    // Setters
+    void set_name(std::string name);
+    void set_rating(std::string rating);
+    void set_watched(int watched);
+    
+    // Incrémente le compteur de visionnage
+    void increment_watched();
+    
+    // Affiche les informations du film
+    void display() const;
+};
+
+#endif // MOVIE_H
